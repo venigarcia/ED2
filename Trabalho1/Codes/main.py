f43b4568cc185f8c603346a8ce4774e834fe6014
Carlos Vinicios Martins Rocha <vinicios_martins2011@hotmail.com>
2019-04-21 15:02:54 -0300

    Finalização do trabalho

--- Trabalho1/Codes/main.py
+++ Trabalho1/Codes/main.py
@@ -2,6 +2,8 @@ from grafo import Grafo
 from algoritmosDeOrdenacao import *
 from utils import *
 import argparse
+import cProfile, pstats, io
+import json
 
 '''
 Implemente o algoritmo de ordenação no arquivo algoritmosDeOrdenacao.py
@@ -10,7 +12,7 @@ Instruções básicas de como fazer a implementação estão no arquivo algoritm
 
 parser = argparse.ArgumentParser(description='Ordenar os nodes do grafo gerado.')
 parser.add_argument('--arquivo', type=str, required=True, dest='arq', help='Caminho para o arquivo que desejas ordenar')
-parser.add_argument('--saida', type=str, required=True, dest='save', help='Caminho para salvar o arquivo ordenado e o Log da execução')
+parser.add_argument('--saida', type=str, dest='save', help='Caminho para salvar o arquivo ordenado e o Log da execução')
 parser.add_argument('--insertionsort', dest='insert', action='store_true', help='Algoritmo de ordenacaoo InsertionSort')
 parser.add_argument('--selectionsort', dest='select', action='store_true', help='Algoritmo de ordenacao SelectionSort')
 parser.add_argument('--shellsort', dest='shell', action='store_true', help='Algoritmo de ordenacao ShellSort')
@@ -23,7 +25,6 @@ parser.add_argument('--pivo', dest='pivo', type=int, default=None, required=Fals
 args = parser.parse_args()
 
 if __name__ == "__main__":
-
     if args.insert:
         algoritimoDeOrdenacao = InsertionSort()
     elif args.select:
@@ -41,10 +42,46 @@ if __name__ == "__main__":
 
     arquivoJson = args.arq
     arquivoDeSaida = args.save
+    print()
+    print(arquivoDeSaida)
+    print()
+    for i in range(len(arquivoDeSaida)-6, -1, -1):
+        if arquivoDeSaida[i] == '\\':
+            break
+    logFile = arquivoDeSaida[-(len(arquivoDeSaida)-1-i):-4]
+    algo = []
+    algos = [InsertionSort(), SelectionSort(), ShellSort(), MergeSort(), QuickSort(), HeapSort(), CountSort()]
+    final_val = []
+    for algo in algos:     
+        pr = cProfile.Profile()
+        pr.enable()
+        
+        grafo = Grafo()
+        grafo.estabelecerAlgoritmoDeOrdencao(algoritimoDeOrdenacao, args.pivo)
+        grafo.carregarGrafo(arquivoJson)
+        arvoreGeradoraMinima =  grafo.executarKruskal()
+        SalvarArvoreGeradoraMinimaEmArquivo(arquivoDeSaida, arvoreGeradoraMinima)
+        
+        pr.disable()
+        s = io.StringIO()
+        ps = pstats.Stats(pr, stream=s).sort_stats('tottime')
+        ps.print_stats()
 
-    grafo = Grafo()
-    grafo.estabelecerAlgoritmoDeOrdencao(algoritimoDeOrdenacao, args.pivo)
-    grafo.carregarGrafo(arquivoJson)
+        out = s.getvalue()
+        count = 0
+        start = 0
+        end = 0
+        for i in range(9, len(out)):
+            if out[i] == ' ':
+                count+=1
+            if count == 4 and start == 0:
+                start = i+1
+            if count == 5 and end == 0:
+                end = i
+                break
+        val=float(out[start:end])
+        final_val.append(val)
 
-    arvoreGeradoraMinima =  grafo.executarKruskal()
-    SalvarArvoreGeradoraMinimaEmArquivo(arquivoDeSaida, arvoreGeradoraMinima)
+    final_dict = {"InsertionSort":final_val[0], "SelectionSort":final_val[1], "ShellSort":final_val[2], "MergeSort":final_val[3], "QuickSort":final_val[4], "HeapSort":final_val[5], "CountSort":final_val[6]}
+    with open("../LogFiles/" + logFile + ".json", 'w+') as f:
+        json.dump(final_dict, f)
---
+++ Trabalho1/Codes/runs.bat
@@ -0,0 +1,9 @@
+@echo off
+
+py main.py --arquivo C:\Users\Desktop\Documents\GitHub\ED2\Trabalho1\Vertices\20000vertices.json --saida C:\Users\Desktop\Documents\GitHub\ED2\Trabalho1\Results\mst20000vertices.txt --insertionsort
+
+py main.py --arquivo C:\Users\Desktop\Documents\GitHub\ED2\Trabalho1\Vertices\30000vertices.json --saida C:\Users\Desktop\Documents\GitHub\ED2\Trabalho1\Results\mst30000vertices.txt --insertionsort
+
+py main.py --arquivo C:\Users\Desktop\Documents\GitHub\ED2\Trabalho1\Vertices\40000vertices.json --saida C:\Users\Desktop\Documents\GitHub\ED2\Trabalho1\Results\mst40000vertices.txt --insertionsort
+
+py main.py --arquivo C:\Users\Desktop\Documents\GitHub\ED2\Trabalho1\Vertices\50000vertices.json --saida C:\Users\Desktop\Documents\GitHub\ED2\Trabalho1\Results\mst50000vertices.txt --insertionsort
